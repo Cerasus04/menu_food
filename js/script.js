c2e39bd2b6226ab857ff8bcce3f6c2b5b34d4e3c
cerasus1004 <cerasus1004@gmail.com>
2021-07-27 11:50:10 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,188 +1,33 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import  tabs from './modules/tabs.js';
+import  timer from './modules/timer.js';
+import  modal from './modules/modal.js';
+import  cards from './modules/cards.js';
+import  forms from './modules/forms.js';
+import  slider from './modules/slider.js';
+import  calc from './modules/calc.js';
+import {openModal} from './modules/modal';
+
 window.addEventListener('DOMContentLoaded', () => {
 
-  //Tabs
-  const tabs =document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+  const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
 
-  function hideTabContent() {
-    tabsContent.forEach(item => {
-      item.classList.add('hide');
-      item.classList.remove('show', 'fade');
-    });
-
-    tabs.forEach(item => {
-      item.classList.remove('tabheader__item_active');
-    });
-  }
-
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add('show', 'fade');
-    tabsContent[i].classList.remove('hide');
-    tabs[i].classList.add('tabheader__item_active');
-  }
-
-  hideTabContent();
-  showTabContent();
-
-  tabsParent.addEventListener('click', (event) => {
-    const target = event.target;
-
-    if (target && target.classList.contains('tabheader__item')) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
-        }
-      });
-    }
+  tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+  timer('.timer', '2021-07-30');
+  modal('[data-modal]', '.modal', modalTimerId);
+  cards();
+  forms('form', modalTimerId);
+  slider({
+    container: '.offer__slider',
+    slide: '.offer__slide',
+    nextArrow: '.offer__slider-next',
+    prewArrow: '.offer__slider-prev',
+    totalCounter: '#total',
+    currentCounter: '#current',
+    wrapper: '.offer__slider-wrapper',
+    field: '.offer__slider-inner'
   });
-
-  //Timer
-  const deadLine = '2021-07-05T12:43:00';
-
-  function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 *24)),
-          hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-          minutes = Math.floor((t / 1000 / 60) % 60),
-          seconds = Math.floor((t / 1000) % 60);
-
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
-    }
-  }
-
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-          days = timer.querySelector('#days'),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timerInterval = setInterval(updateClock, 1000);
-
-    updateClock();
-
-    function updateClock() {
-      const t = getTimeRemaining(endtime);
-
-      days.textContent = getZero(t.days);
-      hours.textContent = getZero(t.hours);
-      minutes.textContent = getZero(t.minutes);
-      seconds.textContent = getZero(t.seconds);
-
-      if (t.total <= 0) {
-        clearInterval(timerInterval);
-      }
-    }
-  }
-
-  setClock('.timer', deadLine);
-
-  //модальное окно
-
-  const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
-
-
-  modalTrigger.forEach(btn => {
-    btn.addEventListener('click', openModal);
-  });
-
-  function closeModal() {
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    // Либо вариант с toggle - но тогда назначить класс в верстке
-    document.body.style.overflow = '';
-  }
-
-  function openModal() {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
-    clearInterval(modalTimerId);
-}
-
-  modalCloseBtn.addEventListener('click', closeModal);
-
-  modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
-          closeModal();
-      }
-  });
-
-  document.addEventListener('keydown', (e) => {
-      if (e.code === "Escape" && modal.classList.contains('show')) {
-          closeModal();
-      }
-  });
-  const modalTimerId = setTimeout(openModal, 3000);
-
-  function showModalByScroll() {
-      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-          openModal();
-          window.removeEventListener('scroll', showModalByScroll);
-      }
-  }
-  window.addEventListener('scroll', showModalByScroll);
-
-  //слайдер
-
-  const slides = document.querySelectorAll('.offer__slide'),
-    prev = document.querySelector('.offer__slider-prev'),
-    next = document.querySelector('.offer__slider-next'),
-    total = document.querySelector('#total'),
-    current = document.querySelector('#current');
-
-  let slideIndex = 1;
-
-  showSlides(slideIndex);
-
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-  } else {
-    total.textContent = slides.length;
-  }
-
-  function showSlides(n) {
-    if(n > slides.length) {
-      slideIndex =1;
-    }
-
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach(item => item.style.display = 'none');
-
-    slides[slideIndex - 1].style.display = 'block';
-
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
-  }
-
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  prev.addEventListener('click', () => plusSlides(-1));
-  next.addEventListener('click', () => plusSlides(1));
-
+  calc();
 });
-
